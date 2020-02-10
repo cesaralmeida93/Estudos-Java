3cef130bb042bc83862c27683d3ed489c58a2417
Cesar <ocesaralmeida93@gmail.com>
2020-02-10 07:39:55 -0300

    finalizada aula sobre wrappers

---
+++ alura-java/conhecendo-java-util/bytebank-herdado-conta/src/br/com/bytebank/banco/test/util/TesteOutrosWrappers.java
@@ -0,0 +1,30 @@
+package br.com.bytebank.banco.test.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TesteOutrosWrappers {
+
+	public static void main(String[] args) {
+
+		Integer idadeRef = Integer.valueOf(29); //autoboxing
+		System.out.println(idadeRef.intValue()); //unboxing
+		
+		Double dRef = Double.valueOf(3.2); //autoboxing
+		System.out.println(dRef.doubleValue()); //unboxing
+		
+		Boolean bRef = Boolean.FALSE; //autoboxing
+		System.out.println(bRef.booleanValue()); //unboxing
+		
+		Number refNumero = Integer.valueOf(29);
+		
+		List<Number> lista = new ArrayList<>();
+		lista.add(10);
+		lista.add(32.6);
+		lista.add(25.6f);
+		
+		System.out.println(lista);
+		
+	}
+
+}
